d193ecfe39961bd10a8241dc5ece30c3992fc077
ThomasGallner <gallner.thomas@gmail.com>
2023-05-26 10:40:01 +0200

    feat: implement min requirements

---
+++ pv-api/Migrations/20230526081436_Initial.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pv_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PvInstallations",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Longitude = table.Column<float>(type: "real", nullable: false),
+                    Latitude = table.Column<float>(type: "real", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false),
+                    OwnerName = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PvInstallations", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductionReports",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProducedWattage = table.Column<float>(type: "real", nullable: false),
+                    HouseholdWattage = table.Column<float>(type: "real", nullable: false),
+                    BatteryWattage = table.Column<float>(type: "real", nullable: false),
+                    GridWattage = table.Column<float>(type: "real", nullable: false),
+                    PvInstallationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductionReports", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ProductionReports_PvInstallations_PvInstallationId",
+                        column: x => x.PvInstallationId,
+                        principalTable: "PvInstallations",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductionReports_PvInstallationId",
+                table: "ProductionReports",
+                column: "PvInstallationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductionReports");
+
+            migrationBuilder.DropTable(
+                name: "PvInstallations");
+        }
+    }
+}
---
+++ pv-api/Program.cs
@@ -0,0 +1,132 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<PvDbContext>(options =>
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]));
+var app = builder.Build();
+
+app.MapPost("/installations", async (PvDbContext dbContext, AddPvInstallationDto pvInstallationDto) =>
+{
+    var newPvInstallation = new PvInstallation
+    {
+        Longitude = pvInstallationDto.Longitude,
+        Latitude = pvInstallationDto.Latitude,
+        Address = pvInstallationDto.Address,
+        OwnerName = pvInstallationDto.OwnerName,
+        Comments = pvInstallationDto.Comments,
+    };
+    await dbContext.PvInstallations.AddAsync(newPvInstallation);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok(newPvInstallation.ID);
+});
+
+app.MapPost("/installations/{id}/deactivate", async (int id, PvDbContext dbContext) =>
+{
+    var installation = await dbContext.PvInstallations.FirstOrDefaultAsync(i => i.ID == id);
+    if (installation is null)
+    {
+        return Results.NotFound();
+    }
+
+    installation.IsActive = false;
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok(installation);
+});
+
+app.MapPost("/installations/{id}/reports", async (int id, PvDbContext dbContext, AddProductionReportDto productionReportDto) =>
+{
+    DateTime currTime = DateTime.UtcNow;
+    DateTime truncTime = DateTime.UtcNow.AddSeconds(-currTime.Second).AddMilliseconds(-currTime.Millisecond);
+
+    var newProductionReport = new ProductionReport
+    {
+        Timestamp = truncTime,
+        ProducedWattage = productionReportDto.ProducedWattage,
+        HouseholdWattage = productionReportDto.HouseholdWattage,
+        BatteryWattage = productionReportDto.BatteryWattage,
+        GridWattage = productionReportDto.GridWattage,
+        PvInstallationId = id
+    };
+
+    var installation = await dbContext.PvInstallations.FirstOrDefaultAsync(i => i.ID == id);
+
+    if (installation == null)
+    {
+        return Results.NotFound();
+    }
+
+    await dbContext.ProductionReports.AddAsync(newProductionReport);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok(installation);
+});
+
+app.MapGet("/installations/{id}/reports", async (int id, DateTime timestamp, int duration, PvDbContext dbContext) =>
+{
+    var installation = await dbContext.PvInstallations.FirstOrDefaultAsync(i => i.ID == id);
+
+    if (installation == null)
+    {
+        return Results.NotFound();
+    }
+
+    var endTimeStamp = timestamp.AddMinutes(duration);
+
+    var reports = dbContext.ProductionReports.Where(r => r.PvInstallationId == id);
+    float producedWattageSum = 0;
+    foreach (ProductionReport report in reports)
+    {
+        if (report.Timestamp >= timestamp && report.Timestamp <= endTimeStamp)
+        {
+            producedWattageSum += report.ProducedWattage;
+        }
+    }
+
+    return Results.Ok(producedWattageSum);
+});
+
+app.Run();
+
+record AddProductionReportDto(float ProducedWattage, float HouseholdWattage, float BatteryWattage, float GridWattage);
+record AddPvInstallationDto(float Longitude, float Latitude, string Address, string OwnerName, string? Comments);
+
+class PvInstallation
+{
+    public int ID { get; set; }
+    public float Longitude { get; set; }
+    public float Latitude { get; set; }
+    [MaxLength(1024)]
+    public string Address { get; set; } = "";
+    [MaxLength(512)]
+    public string OwnerName { get; set; } = "";
+    public bool IsActive { get; set; } = true;
+    public string? Comments { get; set; } = "";
+
+    [JsonIgnore]
+    public List<ProductionReport> ProductionReports { get; set; } = new();
+}
+
+class ProductionReport
+{
+    public int ID { get; set; }
+    public DateTime Timestamp { get; set; }
+    public float ProducedWattage { get; set; }
+    public float HouseholdWattage { get; set; }
+    public float BatteryWattage { get; set; }
+    public float GridWattage { get; set; }
+
+    public int PvInstallationId { get; set; }
+    [JsonIgnore]
+    public PvInstallation? PvInstallation { get; set; }
+}
+
+class PvDbContext : DbContext
+{
+    public PvDbContext(DbContextOptions<PvDbContext> options) : base(options) { }
+    public DbSet<PvInstallation> PvInstallations => Set<PvInstallation>();
+    public DbSet<ProductionReport> ProductionReports => Set<ProductionReport>();
+}
